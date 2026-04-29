21501f3ff55efd170f45818828bd2ab36b2ec74c
harimmatun <harimmatun@gmail.com>
2026-04-29 22:06:19 +0300

    Add auth utils and crud operations

---
+++ crud.py
@@ -0,0 +1,34 @@
+from sqlalchemy.orm import Session
+import models, schemas, auth
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+def create_user(db: Session, user: schemas.UserCreate):
+    hashed_password = auth.get_password_hash(user.password)
+    db_user = models.User(email=user.email, hashed_password=hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def get_task(db: Session, task_id: int):
+    return db.query(models.Task).filter(models.Task.id == task_id).first()
+
+def create_task(db: Session, task: schemas.TaskCreate):
+    db_task = models.Task(**task.model_dump())
+    db.add(db_task)
+    db.commit()
+    db.refresh(db_task)
+    return db_task
+
+def update_task_status(db: Session, task_id: int, status: str):
+    db_task = get_task(db, task_id)
+    if db_task:
+        db_task.status = status
+        db.commit()
+        db.refresh(db_task)
+    return db_task
